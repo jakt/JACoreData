5a26fce6ef7530d5fff3f8c46afdcb0435953323
Eli Liebman <eli.liebman@byjakt.com>
2016-09-08 08:31:56 -0400

    updated to swift 2.3

--- JACoreData/CoreDataStack.swift
+++ JACoreData/CoreDataStack.swift
@@ -41,7 +41,7 @@ public func createInMemoryMainContext(modelStoreName:String, bundles:[NSBundle]?
 extension NSURL {
     
     static func temporaryURL() -> NSURL {
-        return try! NSFileManager.defaultManager().URLForDirectory(NSSearchPathDirectory.CachesDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent(NSUUID().UUIDString)
+        return try! NSFileManager.defaultManager().URLForDirectory(NSSearchPathDirectory.CachesDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true).URLByAppendingPathComponent(NSUUID().UUIDString)!
     }
     
     static var documentsURL: NSURL {
